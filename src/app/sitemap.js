1390d96464c7caac413f443bdf4b952d6cd210e9
SOUVI11 <vishnu.shaji.1216@gmail.com>
2026-03-03 08:24:31 +0530

    Added sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,8 @@
+// app/sitemap.js
+import { MetadataRoute } from 'next';
+
+export default function sitemap() {
+  return [
+    { url: 'https://vishnushaji1216-portfolio.vercel.app/', lastModified: new Date() },
+  ];
+}
